3d6492ba5559af9be83206f57e60db250b47a077
ClaytonRSouza <claytonsrobert@hotmail.com>
2024-11-06 22:32:46 -0300

    Subindo TDE 1 - ivazão tu é foda

---
+++ src/main/java/br/grupointegrado/educacional/dto/AlunoRequestDTO.java
@@ -0,0 +1,4 @@
+package br.grupointegrado.educacional.dto;
+
+public record AlunoRequestDTO(String nome, String email, String matricula, String telefone, String especialidade) {
+}
---
+++ src/main/resources/db/migration/V1__create-table-alunos.sql
@@ -0,0 +1,8 @@
+create table alunos (
+    id int not null primary key auto_increment,
+    nome varchar(100),
+    email varchar(100),
+    matricula varchar(20),
+    telefone varchar(15),
+    especialidade varchar(100)
+);
